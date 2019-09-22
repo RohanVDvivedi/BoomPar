4225d4daf726bbcda0a21b65d23cc9095cea2eac
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-09-23 00:35:56 +0530

    test case modularized and modified a little, that will help, further new features for testing like shutdown and wait for all threads to be destoryed, etc

--- test/test.c
+++ test/test.c
@@ -11,7 +11,9 @@ struct range
 
 void* my_job_function(void* my_int)
 {
-	printf("thread id => %d, this is a job printing => [%d]\n", (int)pthread_self(), *((int*)my_int));
+	int i = (*((int*)my_int));
+	usleep( (10 - i) * 1000 * 1000);
+	printf("thread id => %d, this is a job printing => [%d], local task waiting %d secs\n", (int)pthread_self(), *((int*)my_int), 10-i);
 	(*((int*)my_int)) += 100;
 	return my_int;
 }
@@ -20,29 +22,48 @@ int main()
 {
 	executor* executor_p = get_executor(FIXED_THREAD_COUNT_EXECUTOR /*CACHED_THREAD_POOL_EXECUTOR*/, 8);
 
+	// to store the references to all the jobs that we create
 	array* my_jobs = get_array(10);
 
+	// create jobs
 	for(int i=0; i<10;i++)
 	{
-		int* input_p = ((int*)(malloc(sizeof(int))));
-		(*input_p) = i;
-		job* job_p = get_job(my_job_function, input_p);
+		// create a new job
+		job* job_p = get_job(my_job_function, ((int*)(malloc(sizeof(int)))) );
+
+		// set its input
+		(*((int*)(job_p->input_p))) = i;
+
+		// store it in out array
 		set_element(my_jobs, job_p, i);
-		submit(executor_p, job_p);
+	}
+
+	// submit jobs, one by one
+	for(int i=0; i<10;i++)
+	{
+		submit(executor_p, (job*)get_element(my_jobs, i));
+	}
+
+	// wait for all the jobs, that we know off to finish
+	for(int i=0; i<10;i++)
+	{
+		// get output_p of the i-th job even if we have to wait
+		void* output_p = get_result_or_wait_for_result((job*)get_element(my_jobs, i));
+
+		// and print their results
+		printf("thread %d waited for result, and received => [%d]\n", (int)pthread_self(), *((int*)output_p));
 	}
 
 	for(int i=0; i<10;i++)
 	{
 		job* job_p = (job*)get_element(my_jobs, i);
-		void* output_p = get_result_or_wait_for_result(job_p);
-		printf("thread %d waited for result, and received => [%d]\n", (int)pthread_self(), *((int*)output_p));
-		free(output_p);
+		free(job_p->output_p);
 		delete_job(job_p);
 	}
 
 	delete_array(my_jobs);
 
-	usleep(20 * 1000 * 1000);
+	usleep(12 * 1000 * 1000);
 
 	delete_executor(executor_p);
 
