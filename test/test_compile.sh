53aaf36ff8cd0c546d2bc37914d265fdab7ae457
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-28 19:44:35 +0530

    test function refactored

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,6 +1,6 @@
 cd ..
 make all
 cd test
-gcc test_executor.c -o test_executor.out	-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
-gcc test_job.c 		-o test_job.out			-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
-gcc test_worker.c 	-o test_worker.out		-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
+gcc -Wall test_executor.c 	-o test_executor.out	-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
+gcc -Wall test_job.c 		-o test_job.out			-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
+gcc -Wall test_worker.c 	-o test_worker.out		-I../inc -I../../cutlery/inc -L../bin -L../../cutlery/bin -lpthread -lboompar -lcutlery
--- test/test_executor.c
+++ test/test_executor.c
@@ -11,23 +11,6 @@ void* my_job_function(void* my_int)
 	return my_int;
 }
 
-void* my_job_function_simple(void* my_int)
-{
-	int i = (*((int*)my_int));
-	printf("%d ided Job simple prints => [%d]\n", (int)pthread_self(), i);
-	//usleep(3 * 1000 * 1000);
-	(*((int*)my_int)) += 1000;
-	return my_int;
-}
-
-void* job_waiting(void* job_to_wait)
-{
-	job* job_p = (job*) job_to_wait;
-	int* result = get_result(job_p);
-	printf("%d waiting thread prints => [%d]\n", (int)pthread_self(), (*result));
-	return NULL;
-}
-
 int main()
 {
 	printf("Main thread : %d\n", (int)pthread_self());
@@ -138,71 +121,4 @@ int main()
 	}
 
 	delete_array(my_ints);
-
-	printf("----Testing---- Job async from thread %d\n", (int)pthread_self());printf("LOL\n");
-
-	int input = 5054;
-	job* simple_job_p = get_job(my_job_function_simple, &input);
-
-	printf("Queuing threaded async jobs to wait for the result\n");
-	#define WATING_JOB_COUNT 10
-	job* waiting_jobs[WATING_JOB_COUNT];
-	for(int i = 0; i < WATING_JOB_COUNT; i++)
-	{
-		waiting_jobs[i] = get_job(job_waiting, simple_job_p);
-		execute_async(waiting_jobs[i]);
-	}
-
-	execute_async(simple_job_p);
-
-	//#define RESULT_ANXIOUS_WAITING
-	#define PRINT_WAITING_THREAD_COUNT
-
-	#ifdef RESULT_ANXIOUS_WAITING
-		unsigned long long int result_cyles_anxiously_waiting = 0;
-		while(check_result_ready(simple_job_p) == 0)
-		{
-			result_cyles_anxiously_waiting++;
-		}
-		printf("Anxiously awaited result for %llu clocks\n", result_cyles_anxiously_waiting);
-	#elif defined PRINT_WAITING_THREAD_COUNT
-		unsigned long long int result_cyles_anxiously_waiting = 0;
-		unsigned long long int waiting_threads = 0;
-		while(check_result_ready(simple_job_p) == 0 || get_thread_count_waiting_for_result(simple_job_p) > 0)
-		{
-			result_cyles_anxiously_waiting++;
-			unsigned long long int new_waiting_threads = get_thread_count_waiting_for_result(simple_job_p);
-			if(waiting_threads != new_waiting_threads)
-			{
-				printf("At %llu clocks : %llu -> %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
-				waiting_threads = new_waiting_threads;
-			}
-		}
-
-			unsigned long long int new_waiting_threads = get_thread_count_waiting_for_result(simple_job_p);
-			if(waiting_threads != new_waiting_threads)
-			{
-				printf("At %llu clocks : %llu -> %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
-				waiting_threads = new_waiting_threads;
-			}
-
-		printf("Anxiously awaited result for %llu clocks\n", result_cyles_anxiously_waiting);
-	#endif
-		
-	printf("Deleting threaded async jobs that were asked to wait for the result\n");
-	for(int i = 0; i < WATING_JOB_COUNT; i++)
-	{
-		// wait for waiting job to finish and then delete it
-		printf("Deleting thread %d\n", i);
-		get_result(waiting_jobs[i]);
-		delete_job(waiting_jobs[i]);
-	}
-
-	printf("Parent thread going to get result\n");
-	int* result = get_result(simple_job_p);
-	printf("%d parent thread prints => [%d]\n", (int)pthread_self(), (*result));
-	
-	delete_job(simple_job_p);
-
-	return 0;
 }
