a6170f39e29874f5c20c8d3aba5fd768cc69530f
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-28 20:11:44 +0530

    test executor setup appropriatelt to test all executor features

--- test/test_executor.c
+++ test/test_executor.c
@@ -1,5 +1,4 @@
 #include<executor.h>
-#include<array.h>
 #include<unistd.h>
 
 void* my_job_function(void* my_int)
@@ -11,49 +10,44 @@ void* my_job_function(void* my_int)
 	return my_int;
 }
 
+#define EXECUTOR_TYPE			FIXED_THREAD_COUNT_EXECUTOR /*NEW_THREAD_PER_JOB_SUBMITTED_EXECUTOR*/ /*CACHED_THREAD_POOL_EXECUTOR*/
+#define EXECUTOR_THREADS_COUNT 	4
+
+#define WAIT_FOR_SHUTDOWN       1
+#define SHUTDOWN_IMMEDIATELY 	0
+
+#define TEST_JOBs_COUNT 120
+
+// wait time out on empty job = 3 secs, 50 milli seconds
+#define SECONDS 	3
+#define MILLIS 		50
+#define MICROS 		0
+#define THREAD_TIME_OUT_in_microseconds (SECONDS * 1000000) + (MILLIS * 1000) + (MICROS)
+
 int main()
 {
 	printf("Main thread : %d\n", (int)pthread_self());
 
-	// the number of jobs to submit
-	int jobs_count = 120;
-
-	// the number of threads for executor
-	int threads_count = 4;
-
-	// how do you want to control
-	int wait_for_executors_threads_to_shutdown = 1;
-	int shutdown_immediately = 0;
-
-	// wait time out on empty job = 3 secs, 50 milli seconds
-	unsigned long long int seconds = 3;
-	unsigned long long int milliseconds = 50;
-	unsigned long long int microseconds = 0;
-	unsigned long long int empty_job_queue_wait_time_out_in_micro_seconds = (seconds * 1000000) + (milliseconds * 1000) + (microseconds);
-
-	executor* executor_p = get_executor(FIXED_THREAD_COUNT_EXECUTOR /*NEW_THREAD_PER_JOB_SUBMITTED_EXECUTOR*/ /*CACHED_THREAD_POOL_EXECUTOR*/, threads_count, empty_job_queue_wait_time_out_in_micro_seconds);
+	executor* executor_p = get_executor(EXECUTOR_TYPE, EXECUTOR_THREADS_COUNT, THREAD_TIME_OUT_in_microseconds);
 
 	// to store the references to all the input integers that we create, for each and every job
-	array* my_ints = get_array(jobs_count);
+	int jobs_input_param[TEST_JOBs_COUNT];
 
-	// create jobs
-	for(int i=0; i<jobs_count;i++)
+	// create job parameters
+	for(int i = 0; i < TEST_JOBs_COUNT; i++)
 	{
-		int* input_p = ((int*)(malloc(sizeof(int))));
-
-		(*input_p) = i;
-
-		// store it in our array
-		set_element(my_ints, input_p, i);
+		jobs_input_param[i] = i;
 	}
 
+	// create a submittable job
 	int l = 5001;
-	job* test_submitted_job_p = get_job(my_job_function, &l);
+	job test_submitted_job_p;
+	initialize_job(&test_submitted_job_p, my_job_function, &l);
 
 	// submit jobs, one by one
-	for(int i=0; i<jobs_count;i++)
+	for(int i=0; i < TEST_JOBs_COUNT;i++)
 	{
-		if(submit_function(executor_p, my_job_function, (void*)get_element(my_ints, i)))
+		if(submit_function(executor_p, my_job_function, &jobs_input_param[i]))
 		{
 			//printf("Successfully submitted job with input %d\n", i);
 		}
@@ -63,14 +57,14 @@ int main()
 		}
 
 		// this is to test the functionality of submit_job function
-		if(i == (jobs_count-1)/12)
+		if(i == (TEST_JOBs_COUNT-1)/12)
 		{
-			submit_job(executor_p, test_submitted_job_p);
+			submit_job(executor_p, &test_submitted_job_p);
 		}
 	}
 	printf("finished queueing all jobs\n");
 
-	int* l_p = get_result(test_submitted_job_p);
+	int* l_p = get_result(&test_submitted_job_p);
 	if(l_p != NULL)
 	{
 		printf("the result from test_submitted_job_p : %d, at address %p, while the address of l was %p\n", *l_p, l_p, &l);
@@ -79,18 +73,15 @@ int main()
 	{
 		printf("test_job_p could not be executed\n");
 	}
-	delete_job(test_submitted_job_p);
+	deinitialize_job(&test_submitted_job_p);
 
-	if(wait_for_executors_threads_to_shutdown)
+	printf("Calling shutdown with SHUTDOWN_IMMEDIATELY = %d\n", SHUTDOWN_IMMEDIATELY);
+	shutdown_executor(executor_p, SHUTDOWN_IMMEDIATELY);
+	printf("Shutdown called\n");
+
+	if(WAIT_FOR_SHUTDOWN)
 	{
-		printf("Calling shutdown with shutdown_immediately = %d\n", shutdown_immediately);
-
-		shutdown_executor(executor_p, shutdown_immediately);
-
-		printf("Shutdown called\n");
-
 		printf("Going for waiting on the executor threads to finish\n");
-
 		int wait_error = wait_for_all_threads_to_complete(executor_p);
 		if(wait_error)
 		{
@@ -114,11 +105,13 @@ int main()
 		printf("Deletion of executor failed\n");
 	}
 
-	for(int i=0; i<jobs_count;i++)
+	printf("printing output\n\n");
+	for(int i = 0; i < TEST_JOBs_COUNT; i++)
 	{
-		int* input_p = (int*)get_element(my_ints, i);
-		free(input_p);
+		printf("OUTPUT[%d] = %d\n", i, jobs_input_param[i]);
 	}
 
-	delete_array(my_ints);
+	printf("Test completed\n\n");
+
+	return 0;
 }
