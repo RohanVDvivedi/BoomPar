acfc21adef694b6078f9913d814057e0dfba0149
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-28 21:02:48 +0530

    using test job to precisely test the job functionality

--- test/test_job.c
+++ test/test_job.c
@@ -17,6 +17,12 @@ void* job_waiting(void* job_to_wait)
 	return NULL;
 }
 
+#define WATING_JOB_COUNT 10
+
+// select only one of the below test modes
+//#define RESULT_ANXIOUS_WAITING
+#define PRINT_WAITING_THREAD_COUNT
+
 int main()
 {
 	printf("----Testing---- Job async from thread %d\n", (int)pthread_self());printf("LOL\n");
@@ -24,19 +30,16 @@ int main()
 	int input = 5054;
 	job* simple_job_p = get_job(my_job_function_simple, &input);
 
-	printf("Queuing threaded async jobs to wait for the result\n");
-	#define WATING_JOB_COUNT 10
-	job* waiting_jobs[WATING_JOB_COUNT];
+	printf("Queuing threaded async jobs to wait for the result of the main job\n");
+	
+	job waiting_jobs[WATING_JOB_COUNT];
 	for(int i = 0; i < WATING_JOB_COUNT; i++)
 	{
-		waiting_jobs[i] = get_job(job_waiting, simple_job_p);
-		execute_async(waiting_jobs[i]);
+		initialize_job(&(waiting_jobs[i]), job_waiting, simple_job_p);
+		execute_async(&(waiting_jobs[i]));
 	}
 
 	execute_async(simple_job_p);
-
-	//#define RESULT_ANXIOUS_WAITING
-	#define PRINT_WAITING_THREAD_COUNT
 
 	#ifdef RESULT_ANXIOUS_WAITING
 		unsigned long long int result_cyles_anxiously_waiting = 0;
@@ -54,28 +57,29 @@ int main()
 			unsigned long long int new_waiting_threads = get_thread_count_waiting_for_result(simple_job_p);
 			if(waiting_threads != new_waiting_threads)
 			{
-				printf("At %llu clocks : %llu -> %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
+				printf("At %llu clocks the number of threads waiting for result changed from %llu to %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
 				waiting_threads = new_waiting_threads;
 			}
 		}
 
+			result_cyles_anxiously_waiting++;
 			unsigned long long int new_waiting_threads = get_thread_count_waiting_for_result(simple_job_p);
 			if(waiting_threads != new_waiting_threads)
 			{
-				printf("At %llu clocks : %llu -> %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
+				printf("At %llu clocks the number of threads waiting for result changed from %llu to %llu\n", result_cyles_anxiously_waiting, waiting_threads, new_waiting_threads);
 				waiting_threads = new_waiting_threads;
 			}
 
 		printf("Anxiously awaited result for %llu clocks\n", result_cyles_anxiously_waiting);
 	#endif
 		
-	printf("Deleting threaded async jobs that were asked to wait for the result\n");
+	printf("Deleting/Deinitializing threaded async jobs that were asked to wait for the result\n");
 	for(int i = 0; i < WATING_JOB_COUNT; i++)
 	{
 		// wait for waiting job to finish and then delete it
-		printf("Deleting thread %d\n", i);
-		get_result(waiting_jobs[i]);
-		delete_job(waiting_jobs[i]);
+		printf("Deinitialiing waiting job %d\n", i);
+		get_result(&(waiting_jobs[i]));
+		deinitialize_job(&(waiting_jobs[i]));
 	}
 
 	printf("Parent thread going to get result\n");
