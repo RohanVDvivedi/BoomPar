264b15383deee780bd7503a79b3cbb160001baca
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-30 20:32:33 +0530

    worker is not completely simple and weight less object

--- inc/worker.h
+++ inc/worker.h
@@ -16,59 +16,28 @@ enum worker_job_type
 typedef enum worker_policy worker_policy;
 enum worker_policy
 {
-	WAIT_ON_TIMEDOUT,
+	WAIT_FOREVER_ON_JOB_QUEUE,
 		// again goes to wait, after the job_queue timesout 	
 		// => jobs are blockingly popped from sync job queue
 		// This is policy is to be used when you want to keep on queuing to the worker, forever
 		// Call worker cancel, to stop this thread, later in time
+		// in this policy the job_queue_empty_timeout_in_microseconds parameter is ignored
 
 	KILL_ON_TIMEDOUT,
 		// worker suicides, after the job_queue timesout
 		// => jobs are blockingly popped from sync job queue
 		// This is policy is to be used when you have already queued all the jobs, 
 		// and expect the worker thread to kill itself, once finished
-
-	USE_CALLBACK
-		// uses callback in hopes that new jobs will be added
-		// => jobs are non blockingly popped from sync job queue
-		// This is policy is to be used when you want the worker to notify you using call back, 
-		// to get additional jobs, once it does not find any job
+		// in this policy, the thread will wait for job_queue_empty_timeout_in_microseconds time before killing itself
 };
 
-typedef struct worker worker;
-struct worker
-{
-	// thread_id of the worker thread
-	volatile pthread_t thread_id;
+pthread_t start_worker(sync_queue* job_queue, worker_policy policy, unsigned long long int job_queue_empty_timeout_in_microseconds);
 
-	// job_queue for storing jobs to be executed by the worker
-	sync_queue job_queue;
-
-	// indicates how the worker should respond once the blocking wait has returned with a timedout
-	worker_policy policy;
-
-	// the callback function that gets called when the job_queue of the worker is empty
-	// return 0 			=> 		to return and wait for new tasks
-	// return (non zero) 	=> 		to safely kill the worker
-	void (*job_queue_empty_callback)(worker* wrk, const void* additional_params);
-	const void* additional_params;
-};
-
-worker* get_worker(unsigned long long int size, int is_bounded_queue, long long int job_queue_wait_timeout_in_microseconds, worker_policy policy, void (*job_queue_empty_callback)(worker* wrk, const void* additional_params), const void* additional_params);
-
-void initialize_worker(worker* wrk, unsigned long long int size, int is_bounded_queue, long long int job_queue_wait_timeout_in_microseconds, worker_policy policy, void (*job_queue_empty_callback)(worker* wrk, const void* additional_params), const void* additional_params);
-
-void deinitialize_worker(worker* wrk);
-
-void delete_worker(worker* wrk);
-
-int start_worker(worker* wrk);
-
-int stop_worker(worker* wrk);
+int stop_worker(pthread_t thread_id);
 
 // submit function or job, returns 1 if the job was successfully submitted to the worker
 // function fails and returns 0 if, the job_queue is blocking and it is full 
-int submit_function_to_worker(worker* wrk, void* (*function_p)(void* input_p), void* input_p);
-int submit_job_to_worker(worker* wrk, job* job_p);
+int submit_function(sync_queue* job_queue, void* (*function_p)(void* input_p), void* input_p);
+int submit_job(sync_queue* job_queue, job* job_p);
 
 #endif
--- src/worker.c
+++ src/worker.c
@@ -1,99 +1,51 @@
 #include<worker.h>
 
-worker* get_worker(unsigned long long int size, int is_bounded_queue, long long int job_queue_wait_timeout_in_microseconds, worker_policy policy, void (*job_queue_empty_callback)(worker* wrk, const void* additional_params), const void* additional_params)
+typedef struct worker_thread_params worker_thread_params;
+struct worker_thread_params
 {
-	worker* wrk = (worker*) malloc(sizeof(worker));
-	initialize_worker(wrk, size, is_bounded_queue, job_queue_wait_timeout_in_microseconds, policy, job_queue_empty_callback, additional_params);
-	return wrk;
-}
+	sync_queue* job_queue;
+	worker_policy policy;
+	unsigned long long int job_queue_empty_timeout_in_microseconds;
+};
 
-void initialize_worker(worker* wrk, unsigned long long int size, int is_bounded_queue, long long int job_queue_wait_timeout_in_microseconds, worker_policy policy, void (*job_queue_empty_callback)(worker* wrk, const void* additional_params), const void* additional_params)
+worker_thread_params* get_worker_thread_params(sync_queue* job_queue, worker_policy policy, unsigned long long int job_queue_empty_timeout_in_microseconds)
 {
-	wrk->thread_id = 0;
-	wrk->policy = policy;
-	wrk->job_queue_empty_callback = job_queue_empty_callback;
-	wrk->additional_params = additional_params;
-	initialize_sync_queue(&(wrk->job_queue), size, is_bounded_queue, job_queue_wait_timeout_in_microseconds);
+	worker_thread_params* wtp = (worker_thread_params*) malloc(sizeof(worker_thread_params));
+	wtp->job_queue = job_queue;
+	wtp->policy = policy;
+	wtp->job_queue_empty_timeout_in_microseconds = job_queue_empty_timeout_in_microseconds;
+	return wtp;
 }
 
 static void* worker_function(void* args);
 
-int start_worker(worker* wrk)
+pthread_t start_worker(sync_queue* job_queue, worker_policy policy, unsigned long long int job_queue_empty_timeout_in_microseconds)
 {
-	int return_val = 0;
-	if(wrk->thread_id == 0)
+	pthread_t thread_id;
+	worker_thread_params* wtp = get_worker_thread_params(job_queue, policy, job_queue_empty_timeout_in_microseconds);
+	int return_val = pthread_create(&thread_id, NULL, worker_function, wtp);
+	if(return_val)
 	{
-		return_val = pthread_create(((pthread_t*)(&(wrk->thread_id))), NULL, worker_function, wrk);
-		if(return_val)
-		{
-			printf("error starting worker %d\n", return_val);
-			wrk->thread_id = 0;
-		}
+		printf("Error starting worker : %d\n", return_val);
+	}
+	return thread_id;
+}
+
+int stop_worker(pthread_t thread_id)
+{
+	int return_val = pthread_cancel(thread_id);
+	if(return_val == ESRCH)
+	{
+		printf("Worker thread not found, thread may have exited prior to this call\n");
 	}
 	else
 	{
-		printf("worker already running at %d\n", (int) wrk->thread_id);
+		printf("error stopping worker %d\n", return_val);
 	}
 	return return_val;
 }
 
-int stop_worker(worker* wrk)
-{
-	int return_val = 0;
-	if(wrk->thread_id != 0)
-	{
-		return_val = pthread_cancel(wrk->thread_id);
-		if(return_val == 0)
-		{
-			wrk->thread_id = 0;
-		}
-		else if(return_val == ESRCH)
-		{
-			printf("Worker thread not found, thread may have exited prior to this call\n");
-		}
-		else
-		{
-			printf("error stopping worker %d\n", return_val);
-		}
-	}
-	else
-	{
-		printf("Worker thread already killed\n");
-	}
-	return return_val;
-}
-
-void deinitialize_worker(worker* wrk)
-{
-	job* job_p = NULL;
-
-	// pop all jobs from the queue, and delete them
-	while(!is_empty_sync_queue(&(wrk->job_queue)))
-	{
-		job_p = (job*) pop_sync_queue_non_blocking(&(wrk->job_queue)); 
-
-		// once the job is executed we delete the job, if worker is memory managing the job
-		// i.e. it was a job submitted by the client as a function
-		if(job_p->job_type == JOB_WITH_MEMORY_MANAGED_BY_WORKER)
-		{
-			delete_job(job_p);
-		}
-		else if(job_p->job_type == JOB_WITH_MEMORY_MANAGED_BY_CLIENT)
-		{
-			set_result(job_p, NULL);
-		}
-	}
-
-	deinitialize_sync_queue(&(wrk->job_queue));
-}
-
-void delete_worker(worker* wrk)
-{
-	deinitialize_worker(wrk);
-	free(wrk);
-}
-
-int submit_function_to_worker(worker* wrk, void* (*function_p)(void* input_p), void* input_p)
+int submit_function_to_worker(sync_queue* job_queue, void* (*function_p)(void* input_p), void* input_p)
 {
 	int was_job_queued = 0;
 
@@ -105,7 +57,7 @@ int submit_function_to_worker(worker* wrk, void* (*function_p)(void* input_p), v
 	// if this update of job status is successfull, then only we go forward and queue the job
 	if(job_status_change(job_p, QUEUED))
 	{
-		was_job_queued = push_sync_queue_non_blocking(&(wrk->job_queue), job_p);
+		was_job_queued = push_sync_queue_non_blocking(job_queue, job_p);
 	}
 
 	if(was_job_queued == 0)
@@ -116,7 +68,7 @@ int submit_function_to_worker(worker* wrk, void* (*function_p)(void* input_p), v
 	return was_job_queued;
 }
 
-int submit_job_to_worker(worker* wrk, job* job_p)
+int submit_job_to_worker(sync_queue* job_queue, job* job_p)
 {
 	int was_job_queued = 0;
 
@@ -126,7 +78,7 @@ int submit_job_to_worker(worker* wrk, job* job_p)
 	// if this update of job status is successfull, then only we go forward and queue the job
 	if(job_status_change(job_p, QUEUED))
 	{
-		was_job_queued = push_sync_queue_non_blocking(&(wrk->job_queue), job_p);
+		was_job_queued = push_sync_queue_non_blocking(job_queue, job_p);
 	}
 
 	return was_job_queued;
@@ -136,85 +88,61 @@ int submit_job_to_worker(worker* wrk, job* job_p)
 // dequeuing jobs continuously from the job_queue, to execute them
 static void* worker_function(void* args)
 {
+	worker_thread_params wtp = *((worker_thread_params*)(args));
+	free(args);
+
+	if(wtp.job_queue == NULL)
+	{
+		return NULL;
+	}
+
+	unsigned long long int job_queue_empty_timeout_in_microseconds;
+
+	if(wtp.policy == WAIT_FOREVER_ON_JOB_QUEUE)
+	{
+		job_queue_empty_timeout_in_microseconds = 0;
+	}
+	else if(wtp.policy == KILL_ON_TIMEDOUT)
+	{
+		job_queue_empty_timeout_in_microseconds = wtp.job_queue_empty_timeout_in_microseconds;
+	}
+	else
+	{
+		return NULL;
+	}
+
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-
-	worker* wrk = ((worker*)(args));
-
-	job* job_p = NULL;
 
 	while(1)
 	{
-		while(1)
-		{
-			// if the worker policy is to not USE_CALLBACK, when the job_queue is empty
-			if(wrk->policy != USE_CALLBACK)
-			{
-				// we blocking wait while, while we are outside the cancel disabled statements
-				// this ensures that worker will not pollute any job memory
-				if(wait_while_empty_sync_queue(&(wrk->job_queue)))
-				{
-					break;
-				}
-			}
-
-			// A worker thread can not be cancelled while the worker is dequeuing and executing a job
-			pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-
-			// pop a job from the queue unblockingly, we can not block while we have disableded thread cancellation
-			job_p = (job*) pop_sync_queue_non_blocking(&(wrk->job_queue));
-
-			if(job_p != NULL)
-			{
-				// execute the job that has been popped
-				execute(job_p);
-
-				// once the job is executed we delete the job, if worker is memory managing the job
-				// i.e. it was a job submitted by the client as a function
-				if(job_p->job_type == JOB_WITH_MEMORY_MANAGED_BY_WORKER)
-				{
-					delete_job(job_p);
-				}
-			}
-
-			// Turn on cancelation of the worker thread once the job has been executed and deleted
-			pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-			
-			// if No job was executed, the job_queue being empty, we need to reevaluate according to the assigned policy
-			if(job_p == NULL)
-			{
-				break;
-			}
-		}
-
-		// Behaviour according to policy, if we waited on the job queue and timedout
-		if(wrk->policy == WAIT_ON_TIMEDOUT)
-		{
-			continue;
-		}
-		else if(wrk->policy == KILL_ON_TIMEDOUT)
+		// we blocking wait while the job queue is empty, when we are outside the cancel disabled statements
+		if(wait_while_empty_sync_queue(wtp.job_queue, job_queue_empty_timeout_in_microseconds))
 		{
 			break;
 		}
-		else if(wrk->policy == USE_CALLBACK)
-		{
-			if(wrk->job_queue_empty_callback != NULL)
-			{
-			 	wrk->job_queue_empty_callback(wrk, wrk->additional_params);
-			}
 
-			if(is_empty_sync_queue(&(wrk->job_queue)))
+		// A worker thread can not be cancelled while the worker is dequeuing and executing a job
+		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+		// pop a job from the queue unblockingly, we can not block while we have disableded thread cancellation
+		job* job_p = (job*) pop_sync_queue_non_blocking(wtp.job_queue);
+
+		if(job_p != NULL)
+		{
+			// execute the job that has been popped
+			execute(job_p);
+
+			// once the job is executed we delete the job, if worker is memory managing the job
+			// i.e. it was a job submitted by the client as a function
+			if(job_p->job_type == JOB_WITH_MEMORY_MANAGED_BY_WORKER)
 			{
-				break;
-			}
-			else
-			{
-				continue;
+				delete_job(job_p);
 			}
 		}
-	}
 
-	// thread will no longer be running, so just mark it KILLED
-	wrk->thread_id = 0;
+		// Turn on cancelation of the worker thread once the job has been executed and deleted
+		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	}
 
 	return NULL;
 }
