7078dffc61dbd19e6bd532a7729abe6dc74997f4
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-16 21:42:35 +0530

    removed redundant dependency

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -2,5 +2,5 @@ cd ..
 make all
 cd test
 gcc -Wall test_executor.c -o test_executor.out	-lpthread -lboompar -lcutlery
-gcc -Wall test_job.c 	  -o test_job.out		-lpthread -lboompar -lcutlery
+gcc -Wall test_job.c 	  -o test_job.out		-lpthread -lboompar
 gcc -Wall test_worker.c   -o test_worker.out	-lpthread -lboompar -lcutlery
