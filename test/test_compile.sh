cdefeecdaf392234a74d1350f5c5c53feeb91a74
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-11 09:39:00 +0530

    no need to call make all for compiling

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,6 +1,3 @@
-cd ..
-make all
-cd test
 gcc -Wall test_executor.c -o test_executor.out	-lboompar -lpthread -lcutlery
 gcc -Wall test_job.c 	  -o test_job.out		-lboompar -lpthread -lcutlery
 gcc -Wall test_worker.c   -o test_worker.out	-lboompar -lpthread -lcutlery
