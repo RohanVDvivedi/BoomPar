fda01cb09cfaa9eb06a0d072d5af95ec8208254f
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-24 09:55:23 +0530

    implemented promise for BoomPAr

---
+++ inc/promise.h
@@ -0,0 +1,35 @@
+#ifndef PROMISE_H
+#define PROMISE_H
+
+#include<pthread.h>
+#include<stdlib.h>
+
+typedef struct promise promise;
+struct promise
+{
+	// signifies whether output result is set by the producer threads
+	int output_result_ready;
+
+	void* output_result;
+
+	pthread_mutex_t promise_lock;
+
+	// threads wait on this conditional variable until promise value could be obtained
+	pthread_cond_t promise_wait;
+};
+
+promise* get_promise();
+
+void initialize_promise(promise* p);
+
+// set_result must be called only once after the promise has been initialized or newly created with get_promise function
+void set_promised_result(promise* p, void* res);
+
+// get result will not block 
+void* get_promised_result(promise* p);
+
+void deinitialize_promise(promise* p);
+
+void delete_promise(promise* p);
+
+#endif
---
+++ src/promise.c
@@ -0,0 +1,62 @@
+#include<promise.h>
+
+promise* get_promise()
+{
+	promise* p = malloc(sizeof(promise));
+	initialize_promise(p);
+	return p;
+}
+
+void initialize_promise(promise* p)
+{
+	p->output_result_ready = 0;
+	p->output_result = NULL;
+	pthread_mutex_init(&(p->promise_lock), NULL);
+	pthread_cond_init(&(p->promise_wait), NULL);
+}
+
+void set_promised_result(promise* p, void* res)
+{
+	pthread_mutex_lock(&(p->promise_lock));
+
+	// set the result
+	p->output_result = res;
+
+	// set the result is ready and wake up all the threads that are waiting for the result
+	p->output_result_ready = 1;
+	pthread_cond_broadcast(&(p->promise_wait));
+
+	pthread_mutex_unlock(&(p->promise_lock));
+}
+
+void* get_promised_result(promise* p)
+{
+	pthread_mutex_lock(&(p->promise_lock));
+
+	// wait while the result is not ready
+	while(p->output_result_ready == 0)
+	{
+		pthread_cond_wait(&(p->promise_wait), &(p->promise_lock));
+	}
+
+	// get the result
+	void* res = p->output_result;
+
+	pthread_mutex_unlock(&(p->promise_lock));
+
+	return res;
+}
+
+void deinitialize_promise(promise* p)
+{
+	p->output_result_ready = 0;
+	p->output_result = NULL;
+	pthread_mutex_destroy(&(p->promise_lock));
+	pthread_cond_destroy(&(p->promise_wait));
+}
+
+void delete_promise(promise* p)
+{
+	deinitialize_promise(p);
+	free(p);
+}
