1c9c51fc2b0d9f04d1a00bb01ef5711efea80546
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-12-05 13:07:46 +0530

    minor changes to job testcase to allow detached threads to exit on promise

--- test/test_job.c
+++ test/test_job.c
@@ -25,33 +25,28 @@ int main()
 	printf("----Testing---- Job async from thread %d\n", (int)pthread_self());
 
 	int input = 5054;
-	promise* simple_promise_p = get_promise();
-	job* simple_job_p = get_job(my_job_function_simple, &input, simple_promise_p);
+	promise simple_promise;	initialize_promise(&simple_promise);
+	job simple_job;			initialize_job(&simple_job, my_job_function_simple, &input, &simple_promise);
 
 	printf("Queuing threaded async jobs to wait for the result of the main job\n");
-	// sub mitting multiple job waiting for result for the simple job
+	// submitting multiple jobs to wait for result of the simple job
 	job waiting_jobs[WATING_JOB_COUNT];
 	for(int i = 0; i < WATING_JOB_COUNT; i++)
 	{
-		initialize_job(&(waiting_jobs[i]), job_waiting, simple_promise_p, NULL);
+		initialize_job(&(waiting_jobs[i]), job_waiting, &simple_promise, NULL);
 		execute_async(&(waiting_jobs[i]));
 	}
 
-	execute_async(simple_job_p);
+	execute_async(&simple_job);
 
 	unsigned long long int result_cyles_anxiously_waiting = 0;
-	while(is_promised_result_ready(simple_promise_p) == 0)
-	{
+	while(is_promised_result_ready(&simple_promise) == 0)
 		result_cyles_anxiously_waiting++;
-	}
 	printf("Anxiously awaited result for %llu clocks\n", result_cyles_anxiously_waiting);
 
 	printf("Parent thread going to get result\n");
-	int* result = get_promised_result(simple_promise_p);
+	int* result = get_promised_result(&simple_promise);
 	printf("%d parent thread prints => [%d]\n", (int)pthread_self(), (*result));
-	
-	delete_job(simple_job_p);
-	delete_promise(simple_promise_p);
 
 	return 0;
 }
