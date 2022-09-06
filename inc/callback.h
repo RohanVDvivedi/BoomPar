cbf07e84dc9aa2cbf17b521282dd04dd419e9fc1
RohanVDvivedi <rohandvivedi@gmail.com>
2022-09-06 15:14:29 -0700

    added an imp comment pivoting the project

--- inc/callback.h
+++ inc/callback.h
@@ -1,6 +1,9 @@
 #ifndef CALLBACK_H
 #define CALLBACK_H
 
+// This struct is nothing but a collection of 2 things 1 being the a callback function and the callback params
+// that this callback function gets called with
+
 typedef struct callback callback;
 struct callback
 {
