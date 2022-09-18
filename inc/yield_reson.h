6a68fd47de386a64d1792733f646445bbe6ba007
RohanVDvivedi <rohandvivedi@gmail.com>
2022-09-18 16:16:07 -0700

    added yield reason that will enable a job to wait for a reason to be fulfilled

---
+++ inc/yield_reson.h
@@ -0,0 +1,30 @@
+#ifndef YIELD_REASON_H
+#define YIELD_REASON_H
+
+typedef enum yield_reason_type yield_reason_type;
+struct yield_reason_type
+{
+	TIMED_WAIT,
+	STREAM_READ_WAIT,
+	STREAM_WRITE_WAIT,
+};
+
+typedef struct yield_reason yield_reason;
+struct yield_reason
+{
+	yield_reason_type type;
+	union
+	{
+		struct
+		{
+			stream* wait_for;
+			unsigned int byte_count;
+		};
+		struct
+		{
+			unsigned long long int nano_seconds_to_wait;
+		};
+	};
+};
+
+#endif
