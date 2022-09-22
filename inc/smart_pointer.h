5c3bb07ec044a1d2a7608192e6621fcf689983bf
RohanVDvivedi <rohandvivedi@gmail.com>
2022-09-22 14:57:08 -0700

    added one more utility dispensable method

--- inc/smart_pointer.h
+++ inc/smart_pointer.h
@@ -57,11 +57,21 @@ smart_pointer create_smart_pointer(smart_pointer_builder const * sp_builder);
 // it will increase the reference counter in sp_p->spnt_p and returns (*sp_p)
 smart_pointer duplicate_smart_pointer(const smart_pointer* sp_p);
 
-// NOTE:: A SMART_POINTER MUST ONLY BE CREATED THE ABOVE 2 METHODS, AND AFTER USE IT MUST BE DESTROYED BY THE METHOD BELOW
+// NOTE:: A SMART_POINTER MUST ONLY BE CREATED THE ABOVE 3 METHODS, AND AFTER USE IT MUST BE DESTROYED BY THE METHOD BELOW
 
 // destroy returns 1 if this call freed the data at data_p
 // it will always return 0 for a NULL smart_pointer
 // it will decrement the reference counter and if the reference count is 0 then the smart_pointer sp_p is set to NULL (i.e. data_p and spnt_p are made NULL)
 int destroy_smart_pointer(smart_pointer* sp_p);
+
+// below function can be used to repoint sp_p from its current value of data_p
+// to a new value as found in dup_sp_from_p
+void repoint_smart_pointer(smart_pointer* sp_p, const smart_pointer* dup_sp_from_p);
+
+/* this above function is equivalent to floowing calls (in exact order) ::
+
+		destroy_smart_pointer(sp_p);
+		(*sp_p) = duplicate_smart_pointer(dup_sp_from_p);
+*/
 
 #endif
