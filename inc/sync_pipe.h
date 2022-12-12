799a28c78c0eee0d4529a804972b70f8581dff05
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-12 12:58:35 -0700

    sync_pipe api formalized

---
+++ inc/sync_pipe.h
@@ -0,0 +1,33 @@
+#ifndef SYNC_PIPE_H
+#define SYNC_PIPE_H
+
+typedef struct sync_pipe sync_pipe;
+struct sync_pipe
+{
+	pthread_mutex_t sync_pipe_lock;
+
+	pthread_cond_t sync_pipe_empty;
+
+	pthread_cond_t sync_pipe_full;
+
+	// the capacity of pyp at any instance may not exceed max_capacity
+	unsigned int max_capacity;
+
+	dpipe pyp;
+};
+
+sync_pipe* new_sync_pipe(unsigned int max_capacity);
+
+void initialize_sync_pipe(sync_pipe* spyp);
+
+void deinitialize_sync_pipe(sync_pipe* spyp);
+
+void delete_sync_pipe(sync_pipe* spyp);
+
+// returns number of bytes written from data (always < data_size)
+unsigned int write_to_sync_pipe(sync_pipe* spyp, const void* data, unsigned int data_size);
+
+// returns number of bytes read into data (always < data_size)
+unsigned int read_from_sync_pipe(sync_pipe* spyp, void* data, unsigned int data_size);
+
+#endif
