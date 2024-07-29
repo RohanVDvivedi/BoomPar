9fd9f83e834e3993d6885669462a0efbac07c8ef
RohanVDvivedi <rohandvivedi@gmail.com>
2024-07-29 21:42:44 +0530

    minor function name fix

--- src/promise_completion_default_callback.c
+++ src/promise_completion_default_callback.c
@@ -1,6 +1,6 @@
 #include<promise_completion_default_callbacks.h>
 
-static void push_to_sync_queue_on_promise_completion_callback_fundtion(promise* p, void* callback_param)
+static void push_to_sync_queue_on_promise_completion_callback_function(promise* p, void* callback_param)
 {
 	sync_queue* sq = callback_param;
 	push_sync_queue_blocking(sq, p, 0);
@@ -8,5 +8,5 @@ static void push_to_sync_queue_on_promise_completion_callback_fundtion(promise* 
 
 promise_completion_callback push_to_sync_queue_on_promise_completion_callback(sync_queue* sq)
 {
-	return (promise_completion_callback){.callback_param = sq, .callback_function = push_to_sync_queue_on_promise_completion_callback_fundtion};
+	return (promise_completion_callback){.callback_param = sq, .callback_function = push_to_sync_queue_on_promise_completion_callback_function};
 }
