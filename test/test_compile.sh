a9051d3ef0cb017f70388d58ef3262ecdf045c7c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-31 22:41:56 +0530

    started with adding tests and a way to compile them

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,4 @@
-gcc -Wall test_executor.c -o test_executor.out	-lboompar -lpthread -lcutlery
-gcc -Wall test_job.c 	  -o test_job.out		-lboompar -lpthread -lcutlery
-gcc -Wall test_worker.c   -o test_worker.out	-lboompar -lpthread -lcutlery
+ gcc -Wall test_periodic_job.c -o test_periodic_job.out	-lboompar -lpthread -lcutlery
+#gcc -Wall test_executor.c     -o test_executor.out     -lboompar -lpthread -lcutlery
+#gcc -Wall test_job.c          -o test_job.out          -lboompar -lpthread -lcutlery
+#gcc -Wall test_worker.c       -o test_worker.out       -lboompar -lpthread -lcutlery
