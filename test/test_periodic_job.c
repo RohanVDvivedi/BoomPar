a6ed27fe2cb3c20185737bf3cbec56fd7e7aecf9
RohanVDvivedi <rohandvivedi@gmail.com>
2025-05-31 23:03:05 +0530

    pause, resume, shutdown and change of period is tested

--- test/test_periodic_job.c
+++ test/test_periodic_job.c
@@ -6,6 +6,8 @@
 #include<inttypes.h>
 #include<unistd.h>
 
+uint64_t millis_begin = -1;
+
 uint64_t millis()
 {
 	struct timespec spec;
@@ -13,20 +15,39 @@ uint64_t millis()
 	return timespec_to_milliseconds(spec);
 }
 
-uint64_t millis_begin = -1;
+uint64_t millis_now()
+{
+	return millis() - millis_begin;
+}
 
 void periodic_function(void* t)
 {
-	printf("Hello from periodic function at %"PRIu64"\n", (millis() - millis_begin));
+	printf("Hello from periodic function at %"PRIu64"\n", millis_now());
 }
 
 int main()
 {
 	millis_begin = millis();
-	printf("Test begins at %"PRIu64"\n", millis_begin);
-	periodic_job* pjob = new_periodic_job(periodic_function, NULL, 3000000);
+	printf("Test begins at %"PRIu64" with 0.5 second period\n", millis_begin);
+	periodic_job* pjob = new_periodic_job(periodic_function, NULL, 500000); // start with half a second
 
-	sleep(10);
+	sleep(5);
+
+	printf("Resuming periodic job at %"PRIu64" => %d\n", millis_now(), resume_periodic_job(pjob));
+
+	sleep(5);
+
+	printf("Pausing periodic job at %"PRIu64" => %d\n", millis_now(), pause_periodic_job(pjob));
+
+	sleep(5);
+
+	printf("Resuming periodic job at %"PRIu64" => %d\n", millis_now(), resume_periodic_job(pjob));
+
+	sleep(5);
+
+	printf("Updating period to 1 second at %"PRIu64" => %d\n", millis_now(), update_period_for_periodic_job(pjob, 1000000));
+
+	sleep(5);
 
 	delete_periodic_job(pjob);
 }
