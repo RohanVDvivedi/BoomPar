879df33a87435322764167921872f5a0d957bace
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-02 22:15:23 +0530

    added alarm job test cases

---
+++ test/test_alarm_job.c
@@ -0,0 +1,64 @@
+#include<boompar/alarm_job.h>
+
+#include<posixutils/timespec_utils.h>
+
+#include<stdio.h>
+#include<inttypes.h>
+#include<unistd.h>
+
+uint64_t millis_begin = -1;
+
+uint64_t millis()
+{
+	struct timespec spec;
+	clock_gettime(CLOCK_MONOTONIC, &spec);
+	return timespec_to_milliseconds(spec);
+}
+
+uint64_t millis_now()
+{
+	return millis() - millis_begin;
+}
+
+uint64_t alarm_function(void* t)
+{
+	static uint64_t next_run_period = 0;
+	printf("Hello from alarm function at %"PRIu64"\n", millis_now());
+	next_run_period += 200000ULL;
+
+	if(next_run_period < 2 * 1000000ULL)
+		return BLOCKING;
+	return next_run_period;
+}
+
+int main()
+{
+	millis_begin = millis();
+	printf("Test begins at %"PRIu64" with 0.5 second period\n", millis_begin);
+	alarm_job* ajob = new_periodic_job(alarm_function, NULL);
+
+	sleep(5);
+
+	printf("Waking up periodic job at %"PRIu64" => %d\n", millis_now(), wake_up_alarm_job(ajob));
+
+	sleep(5);
+
+	printf("Pausing periodic job at %"PRIu64" => %d\n", millis_now(), pause_alarm_job(pjob));
+
+	sleep(5);
+
+	printf("Resuming periodic job at %"PRIu64" => %d\n", millis_now(), wake_up_alarm_job(pjob));
+
+	sleep(5);
+
+	printf("Pausing periodic job at %"PRIu64" => %d\n", millis_now(), pause_alarm_job(pjob));
+	wait_for_pause_or_shutdown_of_alarm_job(ajob);
+
+	sleep(5);
+
+	printf("Pausing periodic job at %"PRIu64" => %d\n", millis_now(), shutdown_alarm_job(pjob));
+
+	sleep(5);
+
+	delete_periodic_job(pjob);
+}
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,4 +1,5 @@
-gcc -Wall test_periodic_job.c -o test_periodic_job.out	-lboompar -lpthread -lcutlery
+gcc -Wall test_periodic_job.c -o test_periodic_job.out -lboompar -lpthread -lcutlery
+gcc -Wall test_alarm_job.c -o test_alarm_job.out	   -lboompar -lpthread -lcutlery
 gcc -Wall test_executor.c     -o test_executor.out     -lboompar -lpthread -lcutlery
 gcc -Wall test_job.c          -o test_job.out          -lboompar -lpthread -lcutlery
 gcc -Wall test_worker.c       -o test_worker.out       -lboompar -lpthread -lcutlery
