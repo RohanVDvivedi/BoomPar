11dacbd1683b11bf7dc6fe121289618edb1fc263
RohanVDvivedi <rohandvivedi@gmail.com>
2025-09-12 21:33:47 +0530

    added test case file for resource_usage_limiter

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,5 +1,6 @@
-gcc -Wall test_periodic_job.c -o test_periodic_job.out -lboompar -lpthread -lcutlery
-gcc -Wall test_alarm_job.c -o test_alarm_job.out	   -lboompar -lpthread -lcutlery
-gcc -Wall test_executor.c     -o test_executor.out     -lboompar -lpthread -lcutlery
-gcc -Wall test_job.c          -o test_job.out          -lboompar -lpthread -lcutlery
-gcc -Wall test_worker.c       -o test_worker.out       -lboompar -lpthread -lcutlery
+gcc -Wall test_periodic_job.c     -o test_periodic_job.out     -lboompar -lpthread -lcutlery
+gcc -Wall test_alarm_job.c        -o test_alarm_job.out	       -lboompar -lpthread -lcutlery
+gcc -Wall test_executor.c         -o test_executor.out         -lboompar -lpthread -lcutlery
+gcc -Wall test_job.c              -o test_job.out              -lboompar -lpthread -lcutlery
+gcc -Wall test_worker.c           -o test_worker.out           -lboompar -lpthread -lcutlery
+gcc -Wall test_resource_limiter.c -o test_resource_limiter.out -lboompar -lpthread -lcutlery
---
+++ test/test_resource_limiter.c
@@ -0,0 +1,6 @@
+#include<boompar/resource_usage_limiter.h>
+
+int main()
+{
+	return 0;
+}
