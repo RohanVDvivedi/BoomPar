307e0a4801d867ccf36136f69cb8bd4aa05b1c00
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-09 16:47:08 +0530

    added basic tiber test case

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -4,3 +4,4 @@ gcc -Wall test_executor.c         -o test_executor.out         -lboompar -lpthre
 gcc -Wall test_job.c              -o test_job.out              -lboompar -lpthread -lcutlery
 gcc -Wall test_worker.c           -o test_worker.out           -lboompar -lpthread -lcutlery
 gcc -Wall test_resource_limiter.c -o test_resource_limiter.out -lboompar -lpthread -lcutlery
+gcc -Wall test_tiber.c            -o test_tiber.out            -lboompar -lpthread -lcutlery
---
+++ test/test_tiber.c
@@ -0,0 +1,14 @@
+#include<boompar/tiber.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define EXECUTOR_THREADS_COUNT 	1
+#define MAX_JOB_QUEUE_CAPACITY  1
+
+void main()
+{
+	executor* executor_p = new_executor(FIXED_THREAD_COUNT_EXECUTOR, EXECUTOR_THREADS_COUNT, MAX_JOB_QUEUE_CAPACITY, 1000, NULL, NULL, NULL);
+
+	return 0;
+}
