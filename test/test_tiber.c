ea3b21b13abb55f5cbf659629a54182730ce1b4c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-09 16:47:35 +0530

    --

--- test/test_tiber.c
+++ test/test_tiber.c
@@ -6,7 +6,7 @@
 #define EXECUTOR_THREADS_COUNT 	1
 #define MAX_JOB_QUEUE_CAPACITY  1
 
-void main()
+int main()
 {
 	executor* executor_p = new_executor(FIXED_THREAD_COUNT_EXECUTOR, EXECUTOR_THREADS_COUNT, MAX_JOB_QUEUE_CAPACITY, 1000, NULL, NULL, NULL);
 
