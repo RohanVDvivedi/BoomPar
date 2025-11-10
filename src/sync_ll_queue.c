7311b5ceba7d703b15716b35901a1a579009e801
RohanVDvivedi <rohandvivedi@gmail.com>
2025-11-10 21:27:53 +0530

    new and delete functions implemented

--- src/sync_ll_queue.c
+++ src/sync_ll_queue.c
@@ -1,12 +1,29 @@
 #include<boompar/sync_ll_queue.h>
 
-sync_ll_queue* new_sync_ll_queue(cy_uint node_offset);
+#include<stdlib.h>
+
+sync_ll_queue* new_sync_ll_queue(cy_uint node_offset)
+{
+	sync_ll_queue* sq = malloc(sizeof(sync_ll_queue));
+	if(sq == NULL)
+		return NULL;
+	if(!initialize_sync_ll_queue(sq, node_offset))
+	{
+		free(sq);
+		return NULL;
+	}
+	return sq;
+}
 
 int initialize_sync_ll_queue(sync_ll_queue* sq, cy_uint node_offset);
 
 void deinitialize_sync_ll_queue(sync_ll_queue* sq);
 
-void delete_sync_ll_queue(sync_ll_queue* sq);
+void delete_sync_ll_queue(sync_ll_queue* sq)
+{
+	deinitialize_sync_ll_queue(sq);
+	free(sq);
+}
 
 int is_empty_sync_ll_queue(sync_ll_queue* sq);
 
